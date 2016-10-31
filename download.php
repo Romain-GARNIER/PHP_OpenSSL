b64e9f6b5bfda5b7bf16dfaa8d3c9c615b406e19
Romain G <garnierr10@gmail.com>
2016-10-31 14:25:12 +0100

    Revert "Ajout de l'exercice A"
    This reverts commit e5c8ca92b12e4f3aaa9661a599679fffb082e5fd.

--- download.php
+++
@@ -1,9 +0,0 @@
-<?php
-$File=$_GET['path'];
-  header('Content-Transfer-Encoding: binary'); //Transfert en binaire (fichier)
-  header('Content-Disposition: attachment; filename="'.basename($File).'"'); //Nom du fichier
-  header('Content-Length:'. filesize($File)); //Taille du fichier
-//Envoi du fichier dont le chemin est passé en paramètre
-  readfile($File);
-
- ?>
