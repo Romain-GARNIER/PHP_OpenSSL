5f87fae43b48aa8d350e07b04a5f212be2a8c4de
Damien Rogé <damien.roge.info@gmail.com>
2016-11-02 20:05:59 +0100

    Edit A & B  chemin config
    j'ai commenté mon chemin de config et remis le tien

--- Exercice_A/index.php
+++ Exercice_A/index.php
@@ -13,9 +13,9 @@ if(!empty($_POST)){
 
 	$configargs = array(
 		"config" =>
-		//"C:\\Program Files (x86)\\EasyPHP-Devserver-16.1\\eds-binaries\\php\\php704vc14x86x161003104536\\extras\\ssl\\openssl.cnf"
+		"C:\\Program Files (x86)\\EasyPHP-Devserver-16.1\\eds-binaries\\php\\php704vc14x86x161003104536\\extras\\ssl\\openssl.cnf"
 		//Mon chemin
-		"C:\\Program Files (x86)\\EasyPHP-Devserver-16.1\\eds-binaries\\php\\php5619vc11x86x160928155123\\extras\\ssl\\openssl.cnf"
+		//"C:\\Program Files (x86)\\EasyPHP-Devserver-16.1\\eds-binaries\\php\\php5619vc11x86x160928155123\\extras\\ssl\\openssl.cnf"
 	);
 
 	/* Création de la clé privé */
